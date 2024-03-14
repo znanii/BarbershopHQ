dc0f6819c0b8fb08bd8b713eeea1c1fe3d2d9c03
zhanii <naborznanii@gmail.com>
2024-03-14 00:09:38 -0700

    save the clients in db

--- app.rb
+++ app.rb
@@ -11,9 +11,35 @@ end
 
 class Barber < ActiveRecord::Base
 end
+before do 
+	@barbers = Barber.all
+end
 
 get '/' do
-	@barbers = Barber.all
-
 	erb :index			
+end
+
+get '/blabla' do
+	
+	erb 'BLABLA'
+end
+
+get '/visit' do
+	erb :visit 
+end
+
+post '/visit' do
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+	Client.create   :name => "#{@username}", 
+			:phone=>"#{@phone}", 
+			:datestamp=>"#{@datetime}",
+			:barber=>"#{@barber}",
+			:color=>"#{@color}"
+
+	erb "<h2>Thank you</h2>"
 end
